85562289090a76f9417f133192635030c9de593c
Chantelle Cookey-Gam <s5646762@bournemouth.ac.uk>
2025-06-29 01:57:34 +0100

    made a few more changes

---
+++ StudentManagementApp/Models/Student.cs
@@ -0,0 +1,9 @@
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Course { get; set; }
+    public DateTime EnrollmentDate { get; set; }
+}
