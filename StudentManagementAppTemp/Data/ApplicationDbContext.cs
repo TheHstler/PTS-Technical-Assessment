fe4ef07a54692ed24b4f6649c5ba8e5849a093aa
Chantelle Cookey-Gam <s5646762@bournemouth.ac.uk>
2025-06-29 02:02:21 +0100

    makiing sure they have the same settings

---
+++ StudentManagementAppTemp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+
+
+using Microsoft.EntityFrameworkCore;
+using StudentManagementApp.Models;
+
+namespace StudentManagementApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
